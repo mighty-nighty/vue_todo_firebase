0ef88de715bc8a6fdfa2861ab6a9ea44fad593e9
Dmitry <44381548+mighty-nighty@users.noreply.github.com>
2026-04-01 12:26:27 +0300

    Update firebaseConfig.js

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -1,10 +1,10 @@
 const fireConfig = {
-  apiKey: "AIzaSyBdUXhXTw1lACbkQ9H8Mzwt1Bjh6nqQQVg",
+  apiKey: "",
   authDomain: "vue-test-todo-base.firebaseapp.com",
   databaseURL: "https://vue-test-todo-base.firebaseio.com",
   projectId: "vue-test-todo-base",
   storageBucket: "vue-test-todo-base.appspot.com",
-  messagingSenderId: "693779746606"
+  messagingSenderId: ""
 };
 
 export default fireConfig
